37fd031636467c18c9b0456ca8298992bf2dac35
azimliorg <121174860+azimliorg@users.noreply.github.com>
2022-12-21 16:56:47 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<p>deneme</p>
+<button class="karanlik-mod-butonu">Karanlık Mod</button>
+<style>
+
+body.dark {
+  background-color: black;
+  color: white;
+}
+<style>
+
+
+<script>
+  var buton = document.querySelector(".karanlik-mod-butonu");
+kutucuk.addEventListener("click", function() {
+    document.body.classList.toggle("dark");
+  });
+</script>
